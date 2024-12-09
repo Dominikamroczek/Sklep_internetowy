17cddef7769352e5064d7558b27c533e2611adc8
Dominikamroczek <domimro@onet.eu>
2024-12-09 20:24:57 +0100

    main

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
+
+function showModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "flex"; 
+}
+
+
+function hideModal() {
+    const modal = document.getElementById("modal");
+    modal.style.display = "none"; 
+}
+
+window.addEventListener("click", (event) => {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        hideModal();
+    }
+});
