86edbdd4daa55e60b407d569a3cd93ec292ddd84
Dominikamroczek <domimro@onet.eu>
2024-12-15 18:02:05 +0100

    main

--- main.js
+++ main.js
@@ -1,17 +1,17 @@
-function showSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'flex'
+function showSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'flex';
 }
-function hideSidebar(){
-    const sidebar = document.querySelector('.sidebar')
-    sidebar.style.display = 'none'
+
+function hideSidebar() {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.display = 'none';
 }
 
 function showOnas() {
     const modal = document.getElementById("onas1");
     modal.style.display = "flex"; 
 }
-
 
 function hideOnas() {
     const modal = document.getElementById("onas1");
@@ -21,15 +21,10 @@ function hideOnas() {
 window.addEventListener("click", (event) => {
     const modal = document.getElementById("onas1");
     if (event.target === modal) {
-        hideModal();
+        hideOnas();
     }
 });
-window.addEventListener("click", (event) => {
-    const modal1 = document.getElementById("okno1");
-    if (event.target === modal1) {
-        closeModal1();
-    }
-});
+
 function openModal1(id) {
     document.getElementById(`okno${id}`).style.display = "flex";
 }
